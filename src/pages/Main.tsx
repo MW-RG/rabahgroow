e9b0719fc2f833fa2ca3a555175fa629076bcfe0
park <mwpark@rabahgroow.com>
2023-01-04 02:28:49 +0900

    main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -14,11 +14,12 @@ const Banner = () => {
   const [buttonDisplay, setButtonDisplay] = useState<boolean>(false);
   const [img, setImg] = useState<number>(1);
   const [imgClass, setImgClass] = useState<string>('w-full h-full');
-  const imgLeftHandler = () => { img === 1 ? setImg(5) : setImg(img - 1) }
-  const imgRightHandler = () => { img === 5 ? setImg(1) : setImg(img + 1) }
-  // const slide = useEffect(() => {
-  //   setTimeout(imgRightHandler, 1000 * 30);
-  // }, [img]);
+  const imgLeftHandler = () => { img === 1 ? setImg(4) : setImg(img - 1) }
+  const imgRightHandler = () => { img === 4 ? setImg(1) : setImg(img + 1) }
+  useEffect(() => {
+    setTimeout(imgRightHandler, 1000 * 30);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [img]);
   useEffect(() => {
     setImgClass('w-full h-full animate-bannerImg')
     setTimeout(() => setImgClass('w-full h-full'), 1000)
@@ -31,15 +32,14 @@ const Banner = () => {
       onMouseOut={ () => setButtonDisplay(false) }
       className='relative'
     >
-      ff
       <img alt="banner" src={require(`../assets/images/banner${img}.png`)} className={imgClass} />
       {buttonDisplay
         ? <div>
-            <div className='absolute top-48 left-5 animate-buttonDisplay hover:cursor-pointer'>
-              <MdArrowBackIos className='text-[#E5DFDF]' size="110" onClick={imgLeftHandler}/>
+            <div className='absolute top-[40%] left-5 animate-buttonDisplay hover:cursor-pointer'>
+              <MdArrowBackIos className='text-[#E5DFDF]' size="80" onClick={imgLeftHandler}/>
             </div>
-            <div className='absolute top-48 right-5 animate-buttonDisplay hover:cursor-pointer'>
-              <MdArrowForwardIos className='text-[#E5DFDF]' size="110" onClick={imgRightHandler}/>            
+            <div className='absolute top-[40%] right-5 animate-buttonDisplay hover:cursor-pointer'>
+              <MdArrowForwardIos className='text-[#E5DFDF]' size="80" onClick={imgRightHandler}/>            
             </div>
           </div>
         : null
